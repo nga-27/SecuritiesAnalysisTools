75d62c1dfe289008b4223f689965b15c8286b7bb
nga-27 <48340178+nga-27@users.noreply.github.com>
2019-05-01 18:23:43 -0500

    suppress inclusion of max/min of head&shoulders

--- libs/feature_tools.py
+++ libs/feature_tools.py
@@ -6,7 +6,7 @@ from .features import local_extrema, reconstruct_extrema, remove_duplicates, add
 from .moving_average import exponential_ma
 
 
-def feature_head_and_shoulders(fund: pd.DataFrame, FILTER_SIZE=10):
+def feature_head_and_shoulders(fund: pd.DataFrame, FILTER_SIZE=10, sanitize_dict=True):
     """ 
     Find head and shoulders feature of a reversal 
     Args:
@@ -24,6 +24,10 @@ def feature_head_and_shoulders(fund: pd.DataFrame, FILTER_SIZE=10):
     hs = find_head_shoulders(r) 
     hs = add_daterange(fund, hs, 5)
     hs = remove_empty_keys(hs) 
+    
+    if sanitize_dict:
+        hs.pop('max')
+        hs.pop('min')
     
     return hs, ma
 
