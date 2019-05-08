450d8d478ec94a4332d2a93d744d9b278990a46a
nga-27 <48340178+nga-27@users.noreply.github.com>
2019-05-07 19:06:59 -0500

    nasit to oscs

--- libs/tools/__init__.py
+++ libs/tools/__init__.py
@@ -1,11 +1,10 @@
 from .math_functions import linear_regression, lower_low, higher_high, bull_bear_th, local_minima
-from .clusters import cluster_dates
+from .moving_average import exponential_ma, windowed_ma_list, exponential_ma_list
 
 from .trends import trendline, trendline_deriv, support, resistance 
 from .trends import get_trend, get_trend_analysis, trend_filter
 
 from .true_strength import relative_strength
-from .moving_average import exponential_ma, windowed_ma_list
 
 from .rsi import RSI
 from .ultimate_oscillator import ultimate_oscillator
@@ -15,3 +14,4 @@ from .trends import get_trend, get_trend_analysis
 from .clusters import cluster_oscs
 
 from .macd import mov_avg_convergence_divergence 
+from .macd import export_macd_nasit_signal
