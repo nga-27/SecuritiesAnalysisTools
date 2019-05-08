26bd4a8b20476820095a0b7a6e48d9b93c04f264
nga-27 <48340178+nga-27@users.noreply.github.com>
2019-05-07 20:35:13 -0500

    dependencies + nci + first loop

--- libs/utils/__init__.py
+++ libs/utils/__init__.py
@@ -1,4 +1,3 @@
 from .formatting import name_parser, dir_lister
 from .plotting import dual_plotting, generic_plotting, histogram, bar_chart
 from .nasit import nasit_oscillator_score, nasit_oscillator_signal, nasit_cluster_signal, nasit_cluster_score
-from .nasit import nasit_composite_index
