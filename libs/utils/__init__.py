23b6ec32cf4fa4f47bf436dbf33b335f4982812d
nga-27 <48340178+nga-27@users.noreply.github.com>
2019-05-10 16:32:53 -0500

    added progressbar import class

--- libs/utils/__init__.py
+++ libs/utils/__init__.py
@@ -1,3 +1,4 @@
 from .formatting import name_parser, dir_lister
 from .plotting import dual_plotting, generic_plotting, histogram, bar_chart
 from .nasit import nasit_oscillator_score, nasit_oscillator_signal, nasit_cluster_signal, nasit_cluster_score
+from .progress_bar import ProgressBar
