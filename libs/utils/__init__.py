39f7dc1c016bd20f7f3144287a97e7393052dc2d
nga-27 <48340178+nga-27@users.noreply.github.com>
2019-05-23 18:25:49 -0500

    json output + date formatter

--- libs/utils/__init__.py
+++ libs/utils/__init__.py
@@ -1,4 +1,4 @@
-from .formatting import name_parser, index_extractor, fund_list_extractor, dates_extractor_list, index_appender
+from .formatting import name_parser, index_extractor, fund_list_extractor, dates_extractor_list, date_extractor, index_appender
 from .formatting import configure_temp_dir, remove_temp_dir, create_sub_temp_dir
 from .plotting import dual_plotting, generic_plotting, histogram, bar_chart
 from .nasit import nasit_oscillator_score, nasit_oscillator_signal, nasit_cluster_signal, nasit_cluster_score
