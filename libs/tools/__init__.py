e4a3161661deaac021987cd685afa9ead56b9989
a394szz <ngamell@mmm.com>
2019-08-26 19:13:37 -0500

    cleaning up trends

--- libs/tools/__init__.py
+++ libs/tools/__init__.py
@@ -4,8 +4,7 @@ from .moving_average import exponential_ma, exponential_ma_list, simple_ma_list
 from .moving_average import windowed_ma_list, windowed_ema_list
 from .moving_average import triple_moving_average, moving_average_swing_trade
 
-from .trends import trendline, trendline_deriv, support, resistance 
-from .trends import get_trend, get_trend_analysis, trend_filter
+from .trends import get_trend, get_trend_analysis
 from .trends import get_trendlines_2
 from .resistance_support import find_resistance_support_lines
 
@@ -15,7 +14,6 @@ from .rsi import RSI
 from .ultimate_oscillator import ultimate_oscillator
 from .full_stochastic import full_stochastic
 
-from .trends import get_trend, get_trend_analysis
 from .clusters import cluster_oscs, export_cluster_nasit_signal
 
 from .macd import mov_avg_convergence_divergence 
