f4796020a1c4d3aa6de6037fe7590600abca94ea
nga-27 <namell91@gmail.com>
2019-09-18 21:34:37 -0500

    date for basic-analysis

--- releases/dev.py
+++ releases/dev.py
@@ -51,7 +51,7 @@ from test import test_competitive
 
 ################################
 _VERSION_ = '0.1.18'
-_DATE_REVISION_ = '2019-09-18'
+_DATE_REVISION_ = '2019-09-19'
 ################################
 PROCESS_STEPS_DEV = 9
 
