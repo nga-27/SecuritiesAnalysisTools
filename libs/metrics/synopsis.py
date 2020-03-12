83f43d623ee7c5fd72b7cdd4862c30f096fe8678
a394szz <ngamell@mmm.com>
2020-03-11 20:38:09 -0500

    improved printing

--- libs/metrics/synopsis.py
+++ libs/metrics/synopsis.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from libs.utils import SP500
 
 
@@ -10,16 +12,10 @@ def generate_synopsis(analysis: dict, **kwargs) -> dict:
     if name is None:
         return synopsis
 
-    name2 = SP500.get(name, name)
-
     for period in analysis[name]:
         synopsis[period] = {'tabular': {}, 'metrics': {}}
 
         if (period != 'metadata') and (period != 'synopsis'):
-            if print_out:
-                print("")
-                print(f"Time period: {period} for {name2}")
-                print("")
 
             for metric in analysis[name][period]:
                 if (metric != 'name'):
@@ -29,16 +25,12 @@ def generate_synopsis(analysis: dict, **kwargs) -> dict:
                             for met in mets:
                                 met_str = f"{metric} ({met})"
                                 synopsis[period]['metrics'][met_str] = mets[met][-1]
-                                if print_out:
-                                    tabs = strings_to_tabs(met_str)
-                                    print(
-                                        f"metrics: {met_str} {tabs} {mets[met][-1]}")
                         else:
                             met_str = f"{metric}"
-                            synopsis[period]['metrics'][met_str] = mets[-1]
-                            if print_out:
-                                tabs = strings_to_tabs(met_str)
-                                print(f"metrics: {met_str} {tabs} {mets[-1]}")
+                            if met_str == 'trendlines':
+                                synopsis[period]['metrics'][met_str] = mets
+                            else:
+                                synopsis[period]['metrics'][met_str] = mets[-1]
 
                     mets = analysis[name][period][metric].get('tabular')
                     if mets is not None:
@@ -46,18 +38,11 @@ def generate_synopsis(analysis: dict, **kwargs) -> dict:
                             for met in mets:
                                 met_str = f"{metric} ({met})"
                                 synopsis[period]['tabular'][met_str] = mets[met][-1]
-                                if print_out:
-                                    tabs = strings_to_tabs(met_str)
-                                    print(
-                                        f"tabular: {met_str} {tabs} {mets[met][-1]}")
                         else:
                             met_str = f"{metric}"
                             synopsis[period]['tabular'][met_str] = mets[-1]
-                            if print_out:
-                                tabs = strings_to_tabs(met_str)
-                                print(f"tabular: {met_str} {tabs} {mets[-1]}")
 
-    output_to_terminal(synopsis, print_out=print_out)
+    output_to_terminal(synopsis, print_out=print_out, name=name)
 
     return synopsis
 
@@ -71,7 +56,7 @@ def strings_to_tabs(string: str) -> str:
         tabs = '\t\t\t\t'
     elif len(string) < 15:
         tabs = '\t\t\t\t'
-    elif len(string) < 22:
+    elif len(string) < 23:
         tabs = '\t\t\t'
     elif len(string) < 30:
         tabs = '\t\t'
@@ -81,7 +66,32 @@ def strings_to_tabs(string: str) -> str:
     return tabs
 
 
-def output_to_terminal(synopsis: dict, print_out=False):
+def output_to_terminal(synopsis: dict, print_out=False, **kwargs):
+    name = kwargs.get('name', '')
+    name2 = SP500.get(name, name)
     if print_out:
         for period in synopsis:
-            print(f"period: {period}")
+            if (period != 'metadata') and (period != 'synopsis'):
+                print("\r\n")
+                print(f"Time period: {period} for {name2}")
+                print("")
+
+                print(f"Tabular:\r\n")
+                for tab in synopsis[period]['tabular']:
+                    tabs = strings_to_tabs(tab)
+                    if isinstance(synopsis[period]['tabular'][tab], (str)):
+                        print(
+                            f"{tab} {tabs} {synopsis[period]['tabular'][tab]}")
+                    else:
+                        print(
+                            f"{tab} {tabs} {np.round(synopsis[period]['tabular'][tab], 5)}")
+
+                print(f"\r\nMetrics:\r\n")
+                for met in synopsis[period]['metrics']:
+                    tabs = strings_to_tabs(met)
+                    if isinstance(synopsis[period]['metrics'][met], (str, list)):
+                        print(
+                            f"{met} {tabs} {synopsis[period]['metrics'][met]}")
+                    else:
+                        print(
+                            f"{met} {tabs} {np.round(synopsis[period]['metrics'][met], 5)}")
