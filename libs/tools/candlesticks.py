ad5955959465b19f5ae8b61cbc92461ecba9385d
a394szz <ngamell@mmm.com>
2020-04-11 14:13:22 -0500

    notes and percentiles

--- libs/tools/candlesticks.py
+++ libs/tools/candlesticks.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 
 from libs.utils import candlestick
+from .moving_average import simple_moving_avg
 
 
 def candlesticks(fund: pd.DataFrame, **kwargs) -> dict:
@@ -10,7 +12,8 @@ def candlesticks(fund: pd.DataFrame, **kwargs) -> dict:
     view = kwargs.get('view', '')
     pbar = kwargs.get('progress_bar')
 
-    candle = dict()
+    thresholding_determination(fund)
+    candle = pattern_detection(fund)
 
     if plot_output:
         candlestick(fund, title=name)
@@ -19,3 +22,31 @@ def candlesticks(fund: pd.DataFrame, **kwargs) -> dict:
         candlestick(fund, title=name, filename=filename,
                     saveFig=True)
     return candle
+
+
+def pattern_detection(fund: pd.DataFrame, **kwargs) -> dict:
+
+    patterns = dict()
+
+    return patterns
+
+
+def thresholding_determination(fund: pd.DataFrame):
+
+    open_close = []
+    high_low = []
+    for i, op in enumerate(fund['Open']):
+        open_close.append(np.abs(op - fund['Close'][i]))
+        high_low.append(np.abs(fund['High'][i] - fund['Low'][i]))
+
+    print(
+        f"Open-Close: 35% {np.percentile(open_close, 35)}, 50% {np.percentile(open_close, 50)}, 65% {np.percentile(open_close, 65)}")
+
+
+def day_classification(fund: pd.DataFrame) -> list:
+
+    days = []
+    # classification elements:
+    # current trend (sma-10); short-med-long; black-white; shadow/body ratio;
+    # open, close, high, low (raw)
+    return days
