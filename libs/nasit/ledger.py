1b0102cffeb25748811c882ce6a8bd38d90adf54
a394szz <ngamell@mmm.com>
2020-05-07 17:18:14 -0500

    working!

--- libs/nasit/ledger.py
+++ libs/nasit/ledger.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 from libs.utils import download_data
+from libs.utils import generic_plotting
 
 
 def generate_fund_from_ledger(ledger_name: str):
@@ -15,21 +16,23 @@ def generate_fund_from_ledger(ledger_name: str):
 
     ledger = pd.read_csv(path)
     content = extract_from_format(ledger)
-    print(content['ledger'])
+    content = create_fund(content)
+
+    generic_plotting([content['tabular']])
 
 
 def extract_from_format(ledger: pd.DataFrame) -> dict:
 
     content = {}
     content['title'] = ledger.columns[0]
-    content['start_capital'] = ledger['Unnamed: 1'][1]
+    content['start_capital'] = float(ledger['Unnamed: 1'][1])
     content['start_index'] = find_start_index(ledger, content['title'])
 
     funds, new_ledger = extract_funds(
         ledger, content['start_index'], content['title'])
     content['funds'] = funds
     content['ledger'] = new_ledger
-    content['start_date'] = new_ledger['Date'][content['start_index']]
+    content['start_date'] = new_ledger['Date'][0]
 
     content['start_date'] = datetime.datetime.strptime(
         content['start_date'], "%m/%d/%Y").strftime("%Y-%m-%d")
@@ -69,7 +72,7 @@ def extract_funds(ledger: pd.DataFrame, start_index: int, title: str) -> list:
                    for col in ledger.columns}
     new_ledger = ledger.rename(columns=new_columns)
     new_ledger = new_ledger.drop(list(range(start_index)))
-    print(f"\r\n\r\n{new_ledger}")
+    new_ledger.reset_index(inplace=True)
 
     funds = []
     for ticker in new_ledger['Stock']:
@@ -77,3 +80,73 @@ def extract_funds(ledger: pd.DataFrame, start_index: int, title: str) -> list:
             funds.append(ticker)
 
     return funds, new_ledger
+
+
+def create_fund(content: dict) -> dict:
+    ledger = content['ledger']
+    data = content['raw']
+
+    temp_tick = ledger['Stock'][0]
+    composite = {
+        '_cash_': {
+            'value': [content['start_capital']] * len(data[temp_tick])
+        }
+    }
+
+    for i, ticker in enumerate(ledger['Stock']):
+        t_data = data[ticker]
+
+        if ticker not in composite:
+            composite[ticker] = {
+                'value': [0.0] * len(t_data.index),
+                'shares': [0] * len(t_data.index)
+            }
+
+        action = ledger['Action'][i]
+        date = date_converter(ledger['Date'][i])
+
+        for j, close in enumerate(t_data['Close']):
+
+            if t_data.index[j] == date:
+                if action == 'Buy':
+                    composite[ticker]['shares'][j] += int(ledger['Shares'][i])
+                    composite[ticker]['value'][j] += float(ledger['Shares'][i]) * \
+                        float(ledger['Price of Action'][i])
+                    composite['_cash_']['value'][j] -= float(ledger['Shares'][i]) * \
+                        float(ledger['Price of Action'][i])
+
+                elif action == 'Sell':
+                    composite[ticker]['shares'][j] -= int(ledger['Shares'][i])
+                    composite[ticker]['value'][j] -= float(ledger['Shares'][i]) * \
+                        float(ledger['Price of Action'][i])
+                    composite['_cash_']['value'][j] += float(ledger['Shares'][i]) * \
+                        float(ledger['Price of Action'][i])
+
+            else:
+                if j > 0:
+                    composite[ticker]['shares'][j] = composite[ticker]['shares'][j-1]
+                    composite[ticker]['value'][j] = composite[ticker]['shares'][j] * close
+                    composite['_cash_']['value'][j] = composite['_cash_']['value'][j-1]
+
+    content['details'] = composite
+
+    value = []
+    for i in range(len(data[temp_tick])):
+        val = 0.0
+        for ticker in composite:
+            val += composite[ticker]['value'][i]
+        value.append(val)
+
+    content['tabular'] = value
+
+    return content
+
+
+def date_converter(ledger_date: str, _type='date') -> str:
+    if _type == 'date':
+        new_date = datetime.datetime.strptime(
+            ledger_date, "%m/%d/%Y")
+    elif _type == 'str':
+        new_date = datetime.datetime.strptime(
+            ledger_date, "%m/%d/%Y").strftime("%Y-%m-%d")
+    return new_date
