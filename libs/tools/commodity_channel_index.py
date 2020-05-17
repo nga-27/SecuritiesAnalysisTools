16c43bc721c22e17e596559840479d4376f423a4
a394szz <ngamell@mmm.com>
2020-05-17 15:52:39 -0500

    multiple time frames, features

--- libs/tools/commodity_channel_index.py
+++ libs/tools/commodity_channel_index.py
@@ -15,8 +15,12 @@ def commodity_channel_index(position: pd.DataFrame, **kwargs) -> dict:
 
     cci = dict()
 
+    periods = [20, 40, 80]
+    cci['tabular'] = {}
     cci['tabular'] = generate_commodity_signal(
-        position, plot_output=plot_output, name=name, view=view)
+        position, intervals=periods, plot_output=plot_output, name=name, view=view)
+
+    cci = cci_feature_detection(position, cci)
 
     if p_bar is not None:
         p_bar.uptick(increment=1.0)
@@ -31,51 +35,120 @@ def generate_commodity_signal(position: pd.DataFrame, **kwargs) -> list:
         position {pd.DataFrame} -- fund dataset
 
     Optional Args:
-        interval {int} -- period for simple moving average (default: {20})
+        intervals {list} -- period for simple moving average (default: {[20, 40]})
 
     Returns:
         list -- tabular commodity channel index signal
     """
-    interval = kwargs.get('interval', 20)
+    intervals = kwargs.get('intervals', [10, 20, 40])
     CONSTANT = kwargs.get('constant', .015)
     plot_output = kwargs.get('plot_output', True)
     name = kwargs.get('name', '')
     view = kwargs.get('view', '')
 
-    tabular = []
+    tabular = {str(per): [] for per in intervals}
 
     tps = typical_price_signal(position)
-    sma = simple_moving_avg(tps, interval, data_type='list')
 
-    mean_dev = [0.0] * len(tps)
-    for i in range(interval, len(tps)):
-        sma_val = sma[i]
-        sum_devs = 0.0
-        for j in range(i-interval, i):
-            sum_devs += np.abs(tps[j] - sma_val)
+    for interval in intervals:
+        sma = simple_moving_avg(tps, interval, data_type='list')
 
-        mean_dev[i] = sum_devs / float(interval)
+        mean_dev = [0.0] * len(tps)
+        for i in range(interval, len(tps)):
+            sma_val = sma[i]
+            sum_devs = 0.0
+            for j in range(i-interval, i):
+                sum_devs += np.abs(tps[j] - sma_val)
 
-    for i in range(interval):
-        # Avoid dividing by 0
-        mean_dev[i] = mean_dev[interval]
+            mean_dev[i] = sum_devs / float(interval)
 
-    for i in range(len(tps)):
-        cci = (tps[i] - sma[i]) / (CONSTANT * mean_dev[i])
-        tabular.append(cci)
+        for i in range(interval):
+            # Avoid dividing by 0
+            mean_dev[i] = mean_dev[interval]
+
+        for i in range(len(tps)):
+            cci = (tps[i] - sma[i]) / (CONSTANT * mean_dev[i])
+            tabular[str(interval)].append(cci)
 
     overbought = [100.0 for _ in range(len(tps))]
     oversold = [-100.0 for _ in range(len(tps))]
 
+    plots = [tabular[tab] for tab in tabular]
+    plots.append(overbought)
+    plots.append(oversold)
+
     name2 = SP500.get(name, name)
+    period_strs = ', '.join(intervals)
+    title = f'{name2} - Commodity Channel Index ({period_strs} periods)'
     if plot_output:
-        dual_plotting(position['Close'], [tabular, overbought, oversold],
-                      'Price', 'CCI', title=f'{name2} - Commodity Channel Index')
+        dual_plotting(position['Close'], plots,
+                      'Price', 'CCI', title=title)
 
     else:
         filename = os.path.join(name, view, f"commodity_channel_{name}.png")
-        dual_plotting(position['Close'], [tabular, overbought, oversold],
-                      'Price', 'CCI', title=f'{name2} - Commodity Channel Index',
+        dual_plotting(position['Close'], plots,
+                      'Price', 'CCI', title=title,
                       saveFig=True, filename=filename)
 
     return tabular
+
+
+def cci_feature_detection(position: pd.DataFrame, cci: dict) -> dict:
+
+    cci['features'] = []
+
+    for period in cci['tabular']:
+        cci['features'].extend(get_crossover_features(
+            position, cci['tabular'][period], period))
+
+    return cci
+
+
+def get_crossover_features(position: pd.DataFrame, signal: list, period: str) -> list:
+
+    features = []
+    state = 'x'
+    for i, comp in enumerate(signal):
+        date = position.index[i]
+
+        if state == 'x':
+            if comp > 0.0:
+                state = 'p1'
+            else:
+                state = 'n1'
+
+        elif state == 'n1':
+            if comp > 0.0:
+                state = 'p1'
+            elif comp <= -100.0:
+                state = 'n2'
+                feat = {
+                    "type": 'bearish',
+                    "value": f'crossover to -100 ({period})',
+                    "index": i,
+                    "date": date
+                }
+                features.append(feat)
+
+        elif state == 'p1':
+            if comp < 0.0:
+                state = 'n1'
+            elif comp >= 100.0:
+                state = 'p2'
+                feat = {
+                    "type": 'bullish',
+                    "value": f'crossover to +100 ({period})',
+                    "index": i,
+                    "date": date
+                }
+                features.append(feat)
+
+        elif state == 'p2':
+            if comp < 0.0:
+                state = 'n1'
+
+        elif state == 'n2':
+            if comp > 0.0:
+                state = 'p1'
+
+    return features
