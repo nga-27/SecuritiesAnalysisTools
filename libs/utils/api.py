247f618f379d312bd333d364482352895632732f
Nick Amell <namell91@gmail.com>
2023-01-01 13:23:11 -0600

    clean up of installs

--- libs/utils/api.py
+++ libs/utils/api.py
@@ -74,6 +74,8 @@ def get_api_metadata(fund_ticker: str, **kwargs) -> dict:
     if ticker.info.get('holdings'):
         # ETFs, Mutual Funds, and other indexes will have these but will output an ugly print
         # on financial data below, so let's just return what we have now.
+        api_print += "  Canceled. (Fund is a mutual fund or ETF)"
+        print(f"{REVERSE_LINE}{REVERSE_LINE}{api_print}")
         return metadata
 
     if pb is not None:
